985fed41e59cb67f44ac34d7b8302b512a4eceba
Dokeun Lee <matureelf@gmail.com>
2016-02-12 02:12:26 -0800

    first commit

---
+++ MultiType_GC/Kernel/move.sh
@@ -0,0 +1,4 @@
+mv ./pos.c ./linux-2.6.32/mm/pos.c  &&
+mv ./pos_namespace.c ./linux-2.6.32/mm/pos_namespace.c &&
+mv ./pos.h ./linux-2.6.32/include/linux/pos.h &&
+mv ./pos_namespace.h ./linux-2.6.32/include/linux/pos_namespace.h
