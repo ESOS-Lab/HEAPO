05025e9381ac577cfb91e6358f4163ad96f0ddc3
Dokeun Lee <matureelf@gmail.com>
2016-04-27 02:06:43 -0700

    move

--- MultiType_GC/Kernel/move.sh
+++
@@ -1,4 +0,0 @@
-cp ./pos.c /usr/src/linux-2.6.32/mm/pos.c  &&
-cp ./pos_namespace.c /usr/src/linux-2.6.32/mm/pos_namespace.c &&
-cp ./pos.h /usr/src/linux-2.6.32/include/linux/pos.h &&
-cp ./pos_namespace.h /usr/src/linux-2.6.32/include/linux/pos_namespace.h
--- MultiType_GC/Library/start_l.sh
+++
@@ -1,3 +0,0 @@
-./rm.sh &&
-./install.sh &&
-gcc -o list_test KV/list_test.c -lrt -L/usr/local/lib -lpos -ldb -lm
